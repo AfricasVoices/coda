006c9b570c8a77523b7fab602ad38e9ef8fbedcb
Ana Šemrov <ana.semrov@gmail.com>
2016-09-09 21:42:56 +0100

    Updated readme + the dictionary builder script for easy access from terminal.

--- README.md
+++ README.md
@@ -7,3 +7,25 @@ Prerequisites for running the twitter scripts:
 pip install numpy
 pip install tweepy
 pip install editdistance
+```
+
+Scripts can be run from terminal as follows.
+```bash
+# TWEET COLLECTION
+python collect-tweets.py > tweets_fp
+
+# CLEANING & DICTIONARY BUILDING
+python build-dict.py [tweets_fp] [dict_fp] [resume] [save_freq] 
+# Usage: either enter none of args, the first three, or all of them!
+# Defaults:
+# tweets_fp = "./tweets-intermediate.txt"
+# dict_fp = "./dict.json"
+# resume = "False"
+# save_freq = 1000
+```
+
+Tweet streaming runs for as long as the http connection is up. Usually runs for a few hours with good connection, needs to be manually restarted if there's an connection error.
+
+Dictionary building runs until all tweets have been processed. Intermediate saving is done at a given frequency (# tweets processed). If terminated early, it is possible to resume with building - assuming the dict-intermediate.json was produced.
+
+Dictionary building relies on OrderedDicts to ensure keys are always kept in the same order. This is also important later when building the distance matrix - same index always means the same word.
--- twitterfun/build_dict.py
+++ twitterfun/build_dict.py
@@ -2,66 +2,133 @@ import json
 import cleaners
 import re
 import unicodedata
+import sys
+from collections import OrderedDict
 
-tweet_fp = "./tweets-intermediate.txt"
-dictionary_fp = "./dictionary.json"
 
-tweet_dict = {}
+default_tweet_fp = "./tweets-intermediate.txt"
+default_dictionary_fp = "./dict.json"
+default_save_frequency = 1000
+ask_to_exit = 15000
 
-with open(tweet_fp, 'rb') as tweet_file:
-    counter = 1
-    for line in tweet_file:
 
-        # data format: JSON per line
-        tweet = json.loads(line)
+def clean_and_build(tweets_path, dict_path, save_freq, resume):
+    lines_passed = 0
 
-        for word in tweet["text"]:
-            # TODO ordering of cleaning steps, redundancy
+    if resume:
+        # assume the intermediate dictionary is at intermediate dict_path (dict-intermediate.json)
+        inter_dict_path = dict_path.rstrip(".json")
+        inter_dict_path += "-intermediate.json"
+        tweet_dict = json.load(open(inter_dict_path, "rb"), object_pairs_hook=OrderedDict)
+        resume_from = int(tweet_dict["tweets_processed"]) + 1
+        print "Resuming building from tweet number %d" % resume_from
 
-            word = cleaners.SYMBOLS.sub('', word)  # stupid unicode symbols, arrows, emojis...
-            word = unicodedata.normalize('NFKC', word)  # normalize full width to half width chars
-            word = word.strip(cleaners.STRIP).lower()  # symbols & punctuation from left and right
-            word = word.replace('\'', '')
-            word = re.sub(cleaners.URL_PATTERN, '', word)  # remove URLs
+    else:
+        tweet_dict = OrderedDict([("tweets_processed", 0)])
+        resume_from = 0
+        print "Building dictionary from beggining."
 
-            if any(word.startswith(prefix) for prefix in cleaners.PREFIXES):
+    with open(tweets_path, 'rb') as tweet_file:
+        for line in tweet_file:
+            if lines_passed < resume_from:  # resume processing from where
+                lines_passed += 1
                 continue
 
-            word_split = [w for w in re.split(r'[^\w'+cleaners.SPLIT_EXCEPTIONS+']', word) if len(w) > 1 and not w.isdigit()]
-            filter(lambda word : not any(word.startswith(prefix) for prefix in cleaners.PREFIXES), word_split)
+            # data format: JSON per line
+            tweet = json.loads(line)
 
-            if word_split:
-                if len(word_split) == 1:
-                    w = word_split[0]
-                    if len(w) == 0:
-                        continue
+            for word in tweet["text"]:
+                # TODO ordering of cleaning steps, redundancy
 
-                    if any(w.startswith(prefix) for prefix in cleaners.PREFIXES):
-                        print "la"
-                    if w not in tweet_dict:
-                        tweet_dict[w] = 1
-                    else:
-                        tweet_dict[w] += 1
-                else:
-                    for w in word_split:
+                word = cleaners.SYMBOLS.sub('', word)  # stupid unicode symbols, arrows, emojis...
+                word = unicodedata.normalize('NFKC', word)  # normalize full width to half width chars
+                word = word.strip(cleaners.STRIP).lower()  # symbols & punctuation from left and right
+                word = word.replace('\'', '')
+                word = re.sub(cleaners.URL_PATTERN, '', word)  # remove URLs
 
-                        if any(w.startswith(prefix) for prefix in cleaners.PREFIXES):
-                            print "la"
-                        if w == u'amp':
+                if any(word.startswith(prefix) for prefix in cleaners.PREFIXES):
+                    continue
+
+                word_split = [w for w in re.split(r'[^\w'+cleaners.SPLIT_EXCEPTIONS+']', word) if len(w) > 1 and not w.isdigit()]
+                filter(lambda word : not any(word.startswith(prefix) for prefix in cleaners.PREFIXES), word_split)
+
+                if word_split:
+                    if len(word_split) == 1:
+                        w = word_split[0]
+                        if len(w) == 0:
                             continue
+
+                        # if any(w.startswith(prefix) for prefix in cleaners.PREFIXES):
+
                         if w not in tweet_dict:
                             tweet_dict[w] = 1
                         else:
                             tweet_dict[w] += 1
-        counter += 1
 
-    with open(dictionary_fp, 'wb') as dictionary:
-        json.dump(tweet_dict, dictionary, indent=4)
+                    else:
+                        for w in word_split:
+                            # if any(w.startswith(prefix) for prefix in cleaners.PREFIXES):
+
+                            if w == u'amp':
+                                continue
+                            if w not in tweet_dict:
+                                tweet_dict[w] = 1
+
+                            else:
+                                tweet_dict[w] += 1
+
+            tweet_dict["tweets_processed"] += 1
+
+            if tweet_dict["tweets_processed"] % save_freq == 0:
+                if save(dict_path, tweet_dict, intermediate=True):
+                    print "#tweets processed: %d" % tweet_dict["tweets_processed"]
+
+            if tweet_dict["tweets_processed"] % ask_to_exit == 0:
+                answer = raw_input("%d words currently in dictionary. Save and safely terminate? [y/n]" % len(tweet_dict.keys()))
+                if answer == "y":
+                    save(dict_path, tweet_dict, intermediate=True)
+                    print "*** Safely terminated. Total of %d tweets processed. ***" % tweet_dict["tweets_processed"]
+                    return
+
+        save(dict_path, tweet_dict)  # save to originally given path
+        print "Final dictionary version saved, yay! Total words: %d" % len(tweet_dict.keys())
 
 
+def save(dictionary_path, dictionary_object, intermediate=False):
+    if intermediate:  # build intermediate file name
+        dictionary_path = dictionary_path.rstrip(".json")
+        dictionary_path += "-intermediate.json"
+
+    with open(dictionary_path, 'wb') as dictionary_file:
+        json.dump(dictionary_object, dictionary_file, indent=4)
+    return True
 
 
+if __name__ == '__main__':
+    if len(sys.argv) != 1:
+        # if any args given, pls three at minimum
+        tweet_fp = sys.argv[1]
+        dictionary_fp = sys.argv[2]
+        res = sys.argv[3]
+        if res in ["True", "true", "t", "1", 1]:
+            res = True
+        else:
+            res = False
 
+        if len(sys.argv) == 5:
+            save_frequency = sys.argv[4]
+            if save_frequency.isdigit():
+                save_frequency = int(save_frequency)
 
+        else:
+            save_frequency = default_save_frequency
+            print "Taking the save frequency to be the default - saving every %d tweets processed." % save_frequency
 
+    else:
+        tweet_fp = default_tweet_fp
+        dictionary_fp = default_dictionary_fp
+        save_frequency = default_save_frequency
+        res = False
 
+    clean_and_build(tweets_path=tweet_fp, dict_path=dictionary_fp, save_freq=save_frequency, resume=res)
+    print "*** DONE BUILDING DICTIONARY ***"
