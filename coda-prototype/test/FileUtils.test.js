1f22051f6546967659f8144df877ac2437aa7c5a
Alexander Simpson <as2388@cl.cam.ac.uk>
2017-10-03 15:13:41 +0100

    #74: Add some tests for saving and loading code scheme objects.

--- coda-prototype/test/FileUtils.test.js
+++ coda-prototype/test/FileUtils.test.js
@@ -1,4 +1,5 @@
 /// <reference path="../src/io/FileUtils.ts"/>
+/// <reference path="../model.ts""/>
 /// <reference path="../node_modules/@types/jasmine/index.d.ts" />
 describe("FileUtils.test", function() {
     // Mock saving and loading a file, such that readFileAsText() returns the last object "saved" via saveFile.
@@ -13,18 +14,12 @@ describe("FileUtils.test", function() {
             reader.readAsText(savedFile);
         });
     };
-    it("The mocked saveFile(file) should set savedFile to file", function () {
+    it("should 'mock' FileUtils.saveFile", function() {
         var blob = new Blob(["test"]);
         FileUtils.saveFile(blob);
         expect(savedFile).toBe(blob);
     });
-    it("true should be equal to true", function () {
-        expect(true).toBe(true);
-    });
-    it("true should not be equal to false", function () {
-        expect(true).not.toBe(false);
-    });
-    it("should read the file just saved", function (done) {
+    it("should 'mock' FileUtils.readAsText", function(done) {
         var blob = new Blob(["test"]);
         FileUtils.saveFile(blob);
         FileUtils.readFileAsText(undefined).then(function(out) {
@@ -32,12 +27,78 @@ describe("FileUtils.test", function() {
             done();
         });
     });
-    it("should read the file just saved (sanity check version)", function (done) {
-        var blob = new Blob(["test"]);
-        FileUtils.saveFile(blob);
-        FileUtils.readFileAsText(undefined).then(function(out) {
-            expect(out).not.toBe("different_text");
+    it("should save and load a scheme which has no codes", function(done) {
+        var inScheme = new CodeScheme("id-0", "Scheme0", false);
+        FileUtils.saveCodeScheme(inScheme);
+        FileUtils.loadCodeScheme(undefined).then(function(outScheme) {
+            expect(inScheme).toEqual(outScheme);
             done();
+        }, function(error) {
+            return console.log(error);
+        });
+    });
+    it("should save and load a one-code scheme", function(done) {
+        var inScheme = new CodeScheme("id-1", "Scheme1", false);
+        inScheme.codes.set("code0", new Code(inScheme, "code0", "x", "#ff0000", "", false));
+        FileUtils.saveCodeScheme(inScheme);
+        FileUtils.loadCodeScheme(undefined).then(function(outScheme) {
+            expect(inScheme).toEqual(outScheme);
+            done();
+        }, function(error) {
+            return done.fail(error);
+        });
+    });
+    it("should save and load a multi-code scheme", function(done) {
+        var inScheme = new CodeScheme("27", "Scheme1", false);
+        inScheme.codes.set("27-4", new Code(inScheme, "27-4", "x", "#ff0000", "", false));
+        FileUtils.saveCodeScheme(inScheme);
+        FileUtils.loadCodeScheme(undefined).then(function(outScheme) {
+            expect(inScheme).toEqual(outScheme);
+            done();
+        }, function(error) {
+            return done.fail(error);
+        });
+    });
+    it("should fail if a multi-code scheme has an inconsistent id", function(done) {
+        var inScheme = "scheme_id;scheme_name;code_id;code_value;code_colour;code_shortcut;code_words;code_regex\n" +
+            "66;scheme1;66-91;123;#ffffff;;;\n" +
+            "67;scheme1;67-79;456;#ffffff;;;";
+        FileUtils.saveFile(new Blob([inScheme]));
+        FileUtils.loadCodeScheme(undefined).then(function(outScheme) {
+            done.fail("An inconsistent scheme should have failed");
+        }, function(error) {
+            if (error.name === "CodeConsistencyError")
+                done();
+            else
+                done.fail("Received an error, but it wasn't an expected CodeConsistencyError");
+        });
+    });
+    it("should fail if a multi-code scheme has an inconsistent name", function(done) {
+        var inScheme = "scheme_id;scheme_name;code_id;code_value;code_colour;code_shortcut;code_words;code_regex\n" +
+            "66;scheme1;66-91;123;#ffffff;;;\n" +
+            "66;scheme2;66-79;456;#ffffff;;;";
+        FileUtils.saveFile(new Blob([inScheme]));
+        FileUtils.loadCodeScheme(undefined).then(function(outScheme) {
+            done.fail("An inconsistent scheme should have failed");
+        }, function(error) {
+            if (error.name === "CodeConsistencyError")
+                done();
+            else
+                done.fail("Received an error, but it wasn't an expected CodeConsistencyError");
+        });
+    });
+    it("should fail if a multi-code scheme has codes with the same id", function(done) {
+        var inScheme = "scheme_id;scheme_name;code_id;code_value;code_colour;code_shortcut;code_words;code_regex\n" +
+            "66;scheme1;66-91;123;#ffffff;;;\n" +
+            "66;scheme2;66-91;456;#ffffff;;;";
+        FileUtils.saveFile(new Blob([inScheme]));
+        FileUtils.loadCodeScheme(undefined).then(function(outScheme) {
+            done.fail("An inconsistent scheme should have failed");
+        }, function(error) {
+            if (error.name === "CodeConsistencyError")
+                done(); // TODO: Is this the desired error in this case, or do we need a new one?
+            else
+                done.fail("Received an error, but it wasn't an expected CodeConsistencyError");
         });
     });
 });
--- coda-prototype/test/FileUtils.test.ts
+++ coda-prototype/test/FileUtils.test.ts
@@ -1,4 +1,5 @@
 /// <reference path="../src/io/FileUtils.ts"/>
+/// <reference path="../model.ts""/>
 /// <reference path="../node_modules/@types/jasmine/index.d.ts" />
 
 describe("FileUtils.test", () => {
@@ -15,21 +16,13 @@ describe("FileUtils.test", () => {
         });
     };
 
-    it("The mocked saveFile(file) should set savedFile to file", () => {
+    it("should 'mock' FileUtils.saveFile", () => {
         let blob = new Blob(["test"]);
         FileUtils.saveFile(blob);
         expect(savedFile).toBe(blob);
     });
 
-    it("true should be equal to true", () => {
-        expect(true).toBe(true);
-    });
-
-    it("true should not be equal to false", () => {
-        expect(true).not.toBe(false);
-    });
-
-    it("should read the file just saved", done => {
+    it("should 'mock' FileUtils.readAsText", done => {
         let blob = new Blob(["test"]);
         FileUtils.saveFile(blob);
         FileUtils.readFileAsText(undefined).then(out => {
@@ -38,12 +31,83 @@ describe("FileUtils.test", () => {
         });
     });
 
-    it("should read the file just saved (sanity check version)", done => {
-        let blob = new Blob(["test"]);
-        FileUtils.saveFile(blob);
-        FileUtils.readFileAsText(undefined).then(out => {
-            expect(out).not.toBe("different_text");
+    it("should save and load a scheme which has no codes", done => {
+        let inScheme = new CodeScheme("id-0", "Scheme0", false);
+
+        FileUtils.saveCodeScheme(inScheme);
+        FileUtils.loadCodeScheme(undefined).then(outScheme => {
+            expect(inScheme).toEqual(outScheme);
             done();
+        }, error => console.log(error));
+    });
+
+    it("should save and load a one-code scheme", done => {
+        let inScheme = new CodeScheme("id-1", "Scheme1", false);
+        inScheme.codes.set("code0", new Code(inScheme, "code0", "x", "#ff0000", "", false));
+
+        FileUtils.saveCodeScheme(inScheme);
+        FileUtils.loadCodeScheme(undefined).then(outScheme => {
+            expect(inScheme).toEqual(outScheme);
+            done();
+        }, error => done.fail(error));
+    });
+
+    it("should save and load a multi-code scheme", done => {
+        let inScheme = new CodeScheme("27", "Scheme1", false);
+        inScheme.codes.set("27-4", new Code(inScheme, "27-4", "x", "#ff0000", "", false));
+
+        FileUtils.saveCodeScheme(inScheme);
+        FileUtils.loadCodeScheme(undefined).then(outScheme => {
+            expect(inScheme).toEqual(outScheme);
+            done();
+        }, error => done.fail(error));
+    });
+
+    it("should fail if a multi-code scheme has an inconsistent id", done => {
+        let inScheme =
+            "scheme_id;scheme_name;code_id;code_value;code_colour;code_shortcut;code_words;code_regex\n" +
+            "66;scheme1;66-91;123;#ffffff;;;\n" +
+            "67;scheme1;67-79;456;#ffffff;;;";
+
+        FileUtils.saveFile(new Blob([inScheme]));
+
+        FileUtils.loadCodeScheme(undefined).then(outScheme => {
+            done.fail("An inconsistent scheme should have failed");
+        }, error => {
+            if (error.name === "CodeConsistencyError") done();
+            else done.fail("Received an error, but it wasn't an expected CodeConsistencyError");
+        });
+    });
+
+    it("should fail if a multi-code scheme has an inconsistent name", done => {
+        let inScheme =
+            "scheme_id;scheme_name;code_id;code_value;code_colour;code_shortcut;code_words;code_regex\n" +
+            "66;scheme1;66-91;123;#ffffff;;;\n" +
+            "66;scheme2;66-79;456;#ffffff;;;";
+
+        FileUtils.saveFile(new Blob([inScheme]));
+
+        FileUtils.loadCodeScheme(undefined).then(outScheme => {
+            done.fail("An inconsistent scheme should have failed");
+        }, error => {
+            if (error.name === "CodeConsistencyError") done();
+            else done.fail("Received an error, but it wasn't an expected CodeConsistencyError");
+        });
+    });
+
+    it("should fail if a multi-code scheme has codes with the same id", done => {
+        let inScheme =
+            "scheme_id;scheme_name;code_id;code_value;code_colour;code_shortcut;code_words;code_regex\n" +
+            "66;scheme1;66-91;123;#ffffff;;;\n" +
+            "66;scheme2;66-91;456;#ffffff;;;";
+
+        FileUtils.saveFile(new Blob([inScheme]));
+
+        FileUtils.loadCodeScheme(undefined).then(outScheme => {
+            done.fail("An inconsistent scheme should have failed");
+        }, error => {
+            if (error.name === "CodeConsistencyError") done(); // TODO: Is this the desired error in this case, or do we need a new one?
+            else done.fail("Received an error, but it wasn't an expected CodeConsistencyError");
         });
     });
 });
