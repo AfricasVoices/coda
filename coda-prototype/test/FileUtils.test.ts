86d790b3f1bedd0d28c8fde5085f2c577841c4eb
Alexander Simpson <as2388@cl.cam.ac.uk>
2017-10-03 17:13:32 +0100

    Add regexes and custom words to multi-code scheme case

--- coda-prototype/test/FileUtils.test.ts
+++ coda-prototype/test/FileUtils.test.ts
@@ -2,7 +2,7 @@
 /// <reference path="../model.ts""/>
 /// <reference path="../node_modules/@types/jasmine/index.d.ts" />
 
-describe("FileUtils.test", () => {
+describe("FileUtils", () => {
     // Mock saving and loading a file, such that readFileAsText() returns the last object "saved" via saveFile.
     let savedFile;
     FileUtils.saveFile = function (fileContents: Blob, onDownloadStartedHandler ?: (downloadId: number) => void) {
@@ -54,12 +54,55 @@ describe("FileUtils.test", () => {
         }, error => done.fail(error));
     });
 
-    it("should save and load a multi-code scheme", done => {
+    it("should save and load a simple multi-code scheme", done => {
         let inScheme = new CodeScheme("27", "Scheme1", false);
-        inScheme.codes.set("27-4", new Code(inScheme, "27-4", "x", "#ff0000", "", false));
+        inScheme.codes.set("27-4", new Code(inScheme, "27-4", "Code 1", "#ff0000", "96", false));
+        inScheme.codes.set("27-9", new Code(inScheme, "27-9", "Code 2", "#ff00ff", "97", false));
+        inScheme.codes.set("27-5", new Code(inScheme, "27-5", "Code 3", "#ffff00", "98", false));
+        inScheme.codes.set("27-7", new Code(inScheme, "27-7", "Code 4", "#00ff00", "99", false));
 
         FileUtils.saveCodeScheme(inScheme);
         FileUtils.loadCodeScheme(undefined).then(outScheme => {
+            expect(inScheme).toEqual(outScheme);
+            done();
+        }, error => done.fail(error));
+    });
+
+    it("should save and load a multi-code scheme with regexes", done => {
+        let inScheme = new CodeScheme("27", "Scheme1", false);
+        inScheme.codes.set("27-4", new Code(inScheme, "27-4", "Code 1", "#ff0000", "96", false));
+        inScheme.codes.set("27-9", new Code(inScheme, "27-9", "Code 2", "#ff00ff", "97", false, [".*", "gi"]));
+        inScheme.codes.set("27-5", new Code(inScheme, "27-5", "Code 3", "#ffff00", "98", false, [".*", "g"]));
+        inScheme.codes.set("27-7", new Code(inScheme, "27-7", "Code 4", "#00ff00", "99", false, [".*", "i"]));
+
+        FileUtils.saveCodeScheme(inScheme);
+        FileUtils.loadCodeScheme(undefined).then(outScheme => {
+            expect(inScheme).toEqual(outScheme);
+            done();
+        }, error => done.fail(error));
+    });
+
+    it("should save and load a multi-code scheme with custom words", done => {
+        let inScheme = new CodeScheme("27", "Scheme1", false);
+        let code1 = new Code(inScheme, "27-4", "Code 1", "#ff0000", "96", false);
+        let code2 = new Code(inScheme, "27-9", "Code 2", "#ff00ff", "97", false, [".*", "g"]);
+        let code3 = new Code(inScheme, "27-5", "Code 3", "#ffff00", "98", false, [".*", "g"]);
+        let code4 = new Code(inScheme, "27-7", "Code 4", "#00ff00", "99", false, [".*", "g"]);
+
+        code1.addWords(["some", "test", "words"]);
+        code2.addWords([]);
+        code3.addWords(["abc"]);
+
+        inScheme.codes.set("27-4", code1);
+        inScheme.codes.set("27-9", code2);
+        inScheme.codes.set("27-5", code3);
+        inScheme.codes.set("27-7", code4);
+
+        FileUtils.saveCodeScheme(inScheme);
+        FileUtils.loadCodeScheme(undefined).then(outScheme => {
+            inScheme.codes.forEach(code => delete code._isEdited); // TODO: Understand what Code._isEdited is needed for.
+            outScheme.codes.forEach(code => delete code._isEdited);
+
             expect(inScheme).toEqual(outScheme);
             done();
         }, error => done.fail(error));
@@ -97,7 +140,7 @@ describe("FileUtils.test", () => {
         });
     });
 
-    it("should fail if a multi-code scheme has codes with the same id", done => {
+    it("should fail if a multi-code scheme has codes with the same code id", done => {
         let inScheme =
             "scheme_id;scheme_name;code_id;code_value;code_colour;code_shortcut;code_words;code_regex\n" +
             "66;scheme1;66-91;123;#ffffff;;;\n" +
